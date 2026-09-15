2894f1cc87152f5551245398184548afef77187f
stardiviner <numbchild@gmail.com>
2026-09-15 09:59:41 +0800

    use a tag "contact" to mark org-contacts heading entry as contact

--- org-contacts.el
+++ org-contacts.el
@@ -173,6 +173,11 @@ The following replacements are available:
   "Tags and properties prefix."
   :type 'string)
 
+(defcustom org-contacts-tag "contact"
+  "A tag to mark org-contacts heading entry as contact."
+  :type 'string
+  :safe #'stringp)
+
 (defcustom org-contacts-matcher
   (string-join
    (mapcar (lambda (x) (concat x "<>\"\""))
@@ -273,28 +278,31 @@ Each element has the form (NAME . (FILE . POSITION))."
      (unless (buffer-live-p (get-buffer (file-name-nondirectory file)))
        (find-file-noselect file))
      (with-current-buffer (find-file-noselect file)
-       (org-map-entries
-        (lambda ()
-          (let* ((name (substring-no-properties (org-get-heading t t t t)))
-                 (file (buffer-file-name))
-                 (position (point))
-                 ;; extract properties Org entry headline at `position' as data API for better contacts searching.
-                 (entry-properties (org-entry-properties position 'standard))
-                 (property-name-chinese (cdr (assoc (upcase "NAME(Chinese)")  entry-properties)))
-                 (property-name-english (cdr (assoc (upcase "NAME(English)")  entry-properties)))
-                 (property-nick  (cdr (assoc "NICK" entry-properties)))
-                 (property-email (cdr (assoc "EMAIL" entry-properties)))
-                 ;; (property-mobile (cdr (assoc "MOBILE" entry-properties)))
-                 (property-wechat (cdr (assoc (upcase "WeChat") entry-properties)))
-                 (property-qq (cdr (assoc "QQ" entry-properties))))
-            (list :name name :file file :position position
-                  :name-chinese property-name-chinese
-                  :name-english property-name-english
-                  :nick property-nick
-                  :email property-email
-                  :mobile property-email
-                  :wechat property-wechat
-                  :qq property-qq))))))
+       (delete
+        nil
+        (org-map-entries
+         (lambda ()
+           (when (member org-contacts-tag (org-get-tags (point) 'local))
+             (let* ((name (substring-no-properties (org-get-heading t t t t)))
+                    (file (buffer-file-name))
+                    (position (point))
+                    ;; extract properties Org entry headline at `position' as data API for better contacts searching.
+                    (entry-properties (org-entry-properties position 'standard))
+                    (property-name-chinese (cdr (assoc (upcase "NAME(Chinese)")  entry-properties)))
+                    (property-name-english (cdr (assoc (upcase "NAME(English)")  entry-properties)))
+                    (property-nick  (cdr (assoc "NICK" entry-properties)))
+                    (property-email (cdr (assoc "EMAIL" entry-properties)))
+                    ;; (property-mobile (cdr (assoc "MOBILE" entry-properties)))
+                    (property-wechat (cdr (assoc (upcase "WeChat") entry-properties)))
+                    (property-qq (cdr (assoc "QQ" entry-properties))))
+               (list :name name :file file :position position
+                     :name-chinese property-name-chinese
+                     :name-english property-name-english
+                     :nick property-nick
+                     :email property-email
+                     :mobile property-email
+                     :wechat property-wechat
+                     :qq property-qq))))))))
    (org-contacts-files)))
 
 (defun org-contacts-all-contacts ()
@@ -1792,7 +1800,7 @@ are effectively trimmed.  If nil, all zero-length substrings are retained."
                                 "⨝"))
                  entry (file ,(expand-file-name (car org-contacts-files)))
                  "\
-* %^{NAME}\t\t\t\t%^g
+* %^{NAME}\t\t\t\t:contact:%^g
 :PROPERTIES:
 :ID:   %(org-id-new)
 :DIR:  %\\1
